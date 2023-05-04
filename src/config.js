0214371878c05180bd79674bb2ecb8e69fbb7622
superstar <83311833+clever-web@users.noreply.github.com>
2023-05-04 15:43:32 +0100

    recapcha key fix

--- src/config.js
+++ src/config.js
@@ -62,7 +62,7 @@ export const allLangs = [
 // For netlify
 // export const recaptchaKey = "6Lf2kNYlAAAAABiKOc3ZtWgSNzB6Ll0zros3hDNK";
 // For ollorun.com 
-export const recaptchaKey = "6LfrrdklAAAAAOqwE_jVHk3JIKOe8m-xlA4hm5mc";
+export const recaptchaKey = "6LcjEOAlAAAAANr2yzFFXUklii0Sb4zEnWMWWMyv";
 
 export const GOOGLE_MAP_API_KAY = "AIzaSyBl2oJaWVIAGrzYmMPeHSm0IQnwVm0WXMU";
 // export const GOOGLE_MAP_API_KAY = "AIzaSyAoGoRxR9Rn9VqvzdAnEHO0DScyv-YDqtY";
