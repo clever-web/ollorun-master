4c21b2eccd95ec560e31a00c5fbdf923aae10eb8
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-06 11:16:49 +0100

    Update MotionLazyContainer.js

--- src/components/animate/MotionLazyContainer.js
+++ src/components/animate/MotionLazyContainer.js
@@ -1,19 +1,19 @@
-import PropTypes from 'prop-types';
-import { LazyMotion } from 'framer-motion';
+  import PropTypes from 'prop-types';
+  import { LazyMotion } from 'framer-motion';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-// eslint-disable-next-line import/extensions
-const loadFeatures = () => import('./features.js').then((res) => res.default);
+  // eslint-disable-next-line import/extensions
+  const loadFeatures = () => import('./features.js').then((res) => res.default);
 
-MotionLazyContainer.propTypes = {
-  children: PropTypes.node
-};
+  MotionLazyContainer.propTypes = {
+    children: PropTypes.node
+  };
 
-export default function MotionLazyContainer({ children }) {
-  return (
-    <LazyMotion strict features={loadFeatures}>
-      {children}
-    </LazyMotion>
-  );
-}
+  export default function MotionLazyContainer({ children }) {
+    return (
+      <LazyMotion strict features={loadFeatures}>
+        {children}
+      </LazyMotion>
+    );
+  }
