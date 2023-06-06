6e6ea0fe8cd899c83379aeacfcbdc024b6b10539
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-06 11:16:51 +0100

    Update MotionViewport.js

--- src/components/animate/MotionViewport.js
+++ src/components/animate/MotionViewport.js
@@ -1,36 +1,36 @@
-import PropTypes from 'prop-types';
-import { m } from 'framer-motion';
-// @mui
-import { Box } from '@mui/material';
-// hooks
-import useResponsive from '../../hooks/useResponsive';
-//
-import { varContainer } from '.';
+  import PropTypes from 'prop-types';
+  import { m } from 'framer-motion';
+  // @mui
+  import { Box } from '@mui/material';
+  // hooks
+  import useResponsive from '../../hooks/useResponsive';
+  //
+  import { varContainer } from '.';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-MotionViewport.propTypes = {
-  children: PropTypes.node.isRequired,
-  disableAnimatedMobile: PropTypes.bool,
-};
+  MotionViewport.propTypes = {
+    children: PropTypes.node.isRequired,
+    disableAnimatedMobile: PropTypes.bool,
+  };
 
-export default function MotionViewport({ children, disableAnimatedMobile = true, ...other }) {
-  const isDesktop = useResponsive('up', 'sm');
+  export default function MotionViewport({ children, disableAnimatedMobile = true, ...other }) {
+    const isDesktop = useResponsive('up', 'sm');
 
-  if (!isDesktop && disableAnimatedMobile) {
-    return <Box {...other}>{children}</Box>;
+    if (!isDesktop && disableAnimatedMobile) {
+      return <Box {...other}>{children}</Box>;
+    }
+
+    return (
+      <Box
+        component={m.div}
+        initial="initial"
+        whileInView="animate"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={varContainer()}
+        {...other}
+      >
+        {children}
+      </Box>
+    );
   }
-
-  return (
-    <Box
-      component={m.div}
-      initial="initial"
-      whileInView="animate"
-      viewport={{ once: true, amount: 0.3 }}
-      variants={varContainer()}
-      {...other}
-    >
-      {children}
-    </Box>
-  );
-}
