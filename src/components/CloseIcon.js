589a6369a2aadae5aa95089bcffc02a85da82454
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-07 12:22:04 +0100

    Update CloseIcon.js

--- src/components/CloseIcon.js
+++ src/components/CloseIcon.js
@@ -1,18 +1,18 @@
-// @mui
-import { Box } from "@mui/material";
+    // @mui
+    import { Box } from "@mui/material";
 
-// ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 
-export default function CloseIcon() {
-    return (
-        <Box sx={{
-            position: "absolute",
-            right: "1rem",
-            top: "1rem",
-            zIndex: 2,
-            cursor: "pointer",
-        }}>
-            <img src="/assets/images/closeicon.svg" alt="menuicon" />
-        </Box >
-    )
-}
+    export default function CloseIcon() {
+        return (
+            <Box sx={{
+                position: "absolute",
+                right: "1rem",
+                top: "1rem",
+                zIndex: 2,
+                cursor: "pointer",
+            }}>
+                <img src="/assets/images/closeicon.svg" alt="menuicon" />
+            </Box >
+        )
+    }
