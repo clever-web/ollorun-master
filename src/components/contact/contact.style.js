1f554358b7257041d630e49b88897af7e5ae8a54
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-07 12:22:15 +0100

    Update contact.style.js

--- src/components/contact/contact.style.js
+++ src/components/contact/contact.style.js
@@ -1,9 +1,9 @@
-export const ContactBtnStyle = {
-    background: "#222",
-    color: "#ffffff",
-    fontSize: "clamp(1rem, 1.1vw, 2rem)",
-    width: { xs: "100%", sm: "auto" },
-    "&:hover": {
-        backgroundColor: "#222"
+    export const ContactBtnStyle = {
+        background: "#222",
+        color: "#ffffff",
+        fontSize: "clamp(1rem, 1.1vw, 2rem)",
+        width: { xs: "100%", sm: "auto" },
+        "&:hover": {
+            backgroundColor: "#222"
+        }
     }
-}
