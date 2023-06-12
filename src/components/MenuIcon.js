ff57c8059095711a0ab7a57ed0de4d7401a0d7a6
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-12 14:28:29 +0100

    Update MenuIcon.js

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -1,14 +1,14 @@
-// @mui
-import { Box } from "@mui/material";
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+    // @mui
+    import { Box } from "@mui/material";
+    import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 
-// ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 
-export default function MenuIcon() {
-    return (
-        <Box sx={{display: "flex", alignItems: "center"}}>
-            {/* <img src="/assets/images/menuicon.svg" alt="menuicon" /> */}
-            <KeyboardDoubleArrowRightIcon sx={{width: "32px", height: "32px"}}/>
-        </Box>
-    )
-}
+    export default function MenuIcon() {
+        return (
+            <Box sx={{display: "flex", alignItems: "center"}}>
+                {/* <img src="/assets/images/menuicon.svg" alt="menuicon" /> */}
+                <KeyboardDoubleArrowRightIcon sx={{width: "32px", height: "32px"}}/>
+            </Box>
+        )
+    }
