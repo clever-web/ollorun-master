b21b659dd152f22fca4c8f40afc76a3948fd4573
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-12 14:28:27 +0100

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,26 +1,26 @@
-import PropTypes from "prop-types";
-import { Link as RouterLink } from "react-router-dom";
-// @mui
-import { Box } from "@mui/material";
+  import PropTypes from "prop-types";
+  import { Link as RouterLink } from "react-router-dom";
+  // @mui
+  import { Box } from "@mui/material";
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-Logo.propTypes = {
-  disabledLink: PropTypes.bool,
-  type: PropTypes.string,
-};
+  Logo.propTypes = {
+    disabledLink: PropTypes.bool,
+    type: PropTypes.string,
+  };
 
-export default function Logo({ disabledLink = false, type }) {
+  export default function Logo({ disabledLink = false, type }) {
 
-  const logo = (
-    <Box sx={{width: {xl: '10vw', md: '10vw', sm: '20vw', xs: '30vw'}, minWidth: "130px"}}>
-      <img src="/logo/logo_full.svg" height="auto" alt="logo" />
-    </Box>
-  );
+    const logo = (
+      <Box sx={{width: {xl: '10vw', md: '10vw', sm: '20vw', xs: '30vw'}, minWidth: "130px"}}>
+        <img src="/logo/logo_full.svg" height="auto" alt="logo" />
+      </Box>
+    );
 
-  if (disabledLink) {
-    return <>{logo}</>;
+    if (disabledLink) {
+      return <>{logo}</>;
+    }
+
+    return <RouterLink to="/">{logo}</RouterLink>;
   }
-
-  return <RouterLink to="/">{logo}</RouterLink>;
-}
