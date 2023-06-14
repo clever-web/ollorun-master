035e2dd8ec0d781ea07b428c5a6da9c03fef9155
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-14 02:34:37 +0100

    Update ThemeColorPresets.js

--- src/components/settings/ThemeColorPresets.js
+++ src/components/settings/ThemeColorPresets.js
@@ -1,41 +1,41 @@
-import PropTypes from 'prop-types';
-import { useMemo } from 'react';
-// @mui
-import { alpha, ThemeProvider, createTheme, useTheme } from '@mui/material/styles';
-// hooks
-import useSettings from '../../hooks/useSettings';
-//
-import componentsOverride from '../../theme/overrides';
+  import PropTypes from 'prop-types';
+  import { useMemo } from 'react';
+  // @mui
+  import { alpha, ThemeProvider, createTheme, useTheme } from '@mui/material/styles';
+  // hooks
+  import useSettings from '../../hooks/useSettings';
+  //
+  import componentsOverride from '../../theme/overrides';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-ThemeColorPresets.propTypes = {
-  children: PropTypes.node,
-};
+  ThemeColorPresets.propTypes = {
+    children: PropTypes.node,
+  };
 
-export default function ThemeColorPresets({ children }) {
-  const defaultTheme = useTheme();
+  export default function ThemeColorPresets({ children }) {
+    const defaultTheme = useTheme();
 
-  const { setColor } = useSettings();
+    const { setColor } = useSettings();
 
-  const themeOptions = useMemo(
-    () => ({
-      ...defaultTheme,
-      palette: {
-        ...defaultTheme.palette,
-        primary: setColor,
-      },
-      customShadows: {
-        ...defaultTheme.customShadows,
-        primary: `0 8px 16px 0 ${alpha(setColor.main, 0.24)}`,
-      },
-    }),
-    [setColor, defaultTheme]
-  );
+    const themeOptions = useMemo(
+      () => ({
+        ...defaultTheme,
+        palette: {
+          ...defaultTheme.palette,
+          primary: setColor,
+        },
+        customShadows: {
+          ...defaultTheme.customShadows,
+          primary: `0 8px 16px 0 ${alpha(setColor.main, 0.24)}`,
+        },
+      }),
+      [setColor, defaultTheme]
+    );
 
-  const theme = createTheme(themeOptions);
+    const theme = createTheme(themeOptions);
 
-  theme.components = componentsOverride(theme);
+    theme.components = componentsOverride(theme);
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-}
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  }
