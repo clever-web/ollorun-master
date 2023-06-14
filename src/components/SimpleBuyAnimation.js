478856fac3a63c8fe520e17d0379f7488f98935e
superstar <83311833+clever-web@users.noreply.github.com>
2023-06-14 02:34:46 +0100

    Update SimpleBuyAnimation.js

--- src/components/SimpleBuyAnimation.js
+++ src/components/SimpleBuyAnimation.js
@@ -1,43 +1,43 @@
-import { Box } from "@mui/material";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import useLocales from "../hooks/useLocales";
+  import { Box } from "@mui/material";
+  import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+  import useLocales from "../hooks/useLocales";
 
-export default function SimpleBuyAnimation({ isNavOpen }) {
-  const { translate } = useLocales();
+  export default function SimpleBuyAnimation({ isNavOpen }) {
+    const { translate } = useLocales();
 
-  return (
+    return (
 
-    <Box
-      className="flip-card"
-      onClick={() => window.open("https://flashxchanger.com", "_blank")}
-      sx={{
-        position: "fixed",
-        bottom: "100px",
-        right: "24px",
-        transition: "opacity ease 500ms",
-        opacity: "1",
-        zIndex: "999",
-        cursor: "pointer",
-        display: isNavOpen ? "none !important" : "block !important"
-      }}
-    >
-      <Box className="flip-card-inner" sx={{ maxWidth: "45px", width: "100%" }}>
-        <Box
-          className="flip-card-front"
-          sx={{ p: 0.5, borderRadius: "50%", }}
-        >
-          <ShoppingCartIcon sx={{ fontSize: "34px", color: "#222" }} />
+      <Box
+        className="flip-card"
+        onClick={() => window.open("https://flashxchanger.com", "_blank")}
+        sx={{
+          position: "fixed",
+          bottom: "100px",
+          right: "24px",
+          transition: "opacity ease 500ms",
+          opacity: "1",
+          zIndex: "999",
+          cursor: "pointer",
+          display: isNavOpen ? "none !important" : "block !important"
+        }}
+      >
+        <Box className="flip-card-inner" sx={{ maxWidth: "45px", width: "100%" }}>
+          <Box
+            className="flip-card-front"
+            sx={{ p: 0.5, borderRadius: "50%", }}
+          >
+            <ShoppingCartIcon sx={{ fontSize: "34px", color: "#222" }} />
+          </Box>
+          <Box
+            className="flip-card-back"
+            sx={{ p: 0.5, borderRadius: "50%" }}
+          >
+            <ShoppingCartIcon sx={{ fontSize: "34px", color: "#222" }} />
+          </Box>
         </Box>
-        <Box
-          className="flip-card-back"
-          sx={{ p: 0.5, borderRadius: "50%" }}
-        >
-          <ShoppingCartIcon sx={{ fontSize: "34px", color: "#222" }} />
+        <Box component="p" className="simple-animation-text">
+          {translate("buy_oztg")}
         </Box>
       </Box>
-      <Box component="p" className="simple-animation-text">
-        {translate("buy_oztg")}
-      </Box>
-    </Box>
-  );
-}
+    );
+  }
